ec28c7c485df8c90f016e750dfd9e60368f4827c
Adrián Ángeles <aangeles@gmail.com>
2019-07-25 11:00:45 +0200

    Initial Configuration provided by https://start.spring.io/

---
+++ RestRespository/src/main/kotlin/com/aar/examples/RestRespository/RestControllerApplication.kt
@@ -0,0 +1,11 @@
+package com.aar.examples.RestRespository
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestControllerApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestControllerApplication>(*args)
+}
---
+++ RestRespository/src/test/kotlin/com/aar/examples/RestRespository/RestControllerApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.aar.examples.RestRespository
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class RestControllerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
