d4313120610fcee11c430e6ed4e59bf674600979
Adrián Ángeles <aangeles@gmail.com>
2019-07-31 11:23:44 +0200

    Thymeleaf form

--- RestRespository/src/main/kotlin/com/aar/examples/basickotlinapp/ThymeleafController.kt
+++ RestRespository/src/main/kotlin/com/aar/examples/basickotlinapp/ThymeleafController.kt
@@ -2,16 +2,27 @@ package com.aar.examples.basickotlinapp
 
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.*
+
+data class MyForm(val name: String, val country: String)
 
 @Controller
 class ThymeleafController {
-
 
     @RequestMapping("/template/{name}")
     fun listStudents(@PathVariable("name") name: String, model: Model): String {
         model.addAttribute("name", name)
         return "defaultTemplate"
+    }
+
+    @GetMapping("/myform")
+    fun myForm(model: Model): String {
+        model.addAttribute("myform", MyForm("Nombre", "ES"))
+        return "formTemplate"
+    }
+
+    @PostMapping("/myform")
+    fun myFormSubmit(@ModelAttribute myForm: MyForm): String {
+        return "formTemplateResult.html"
     }
 }
